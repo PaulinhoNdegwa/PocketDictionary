80851fd172c2b0b6f6d16e4e229b7ebd9d3d51b3
Paul Gichuki <pherndegz@gmail.com>
2021-08-12 17:14:47 +0300

    Stick Header component to top onscroll

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <div className="max-w-5xl w-full sm:mx-auto py-2 px-6">
+        <div className="sticky top-0 bg-white max-w-5xl w-full sm:mx-auto py-2 px-6">
             <h3 className="font-bold text-indigo-700 uppercase">Introducing...🥁</h3>
             <p className="text-lg sm:text-2xl font-extrabold text-gray-800">Pocket Dictionary</p>
             <hr className="mt-1" />
