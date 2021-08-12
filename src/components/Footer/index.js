e554b680c519fc35ac52fea699ea1a34592808b8
Paul Gichuki <pherndegz@gmail.com>
2021-08-12 10:12:41 +0300

    Setup tailwindcss, header and footer sections

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return ( 
+        <div className="max-w-2xl mx-auto text-center my-2">
+            <footer>&copy; Copyright 2021 Raul</footer>
+        </div>
+     );
+}
+ 
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div className="max-w-2xl mx-auto border-sm border-gray-500 shadow-lg rounded-lg h-screen p-6">
+            <h3 className="font-bold text-black uppercase">Introducing...🥁</h3>
+            <p className="text-lg sm:text-2xl font-extrabold text-gray-600">Pocket Dictionary</p>
+            <hr className="my-2" />
+        </div>
+    );
+}
+
+export default Header;
